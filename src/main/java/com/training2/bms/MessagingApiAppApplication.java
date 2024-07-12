e377701ac66c9157fe3665670070aa26ce1b221d
Rhuthika H M <rhhm@dhcp-10-120-134-255.wifi.oracle.com>
2024-07-12 15:11:06 +0530

    port change

--- src/main/java/com/training2/bms/MessagingApiAppApplication.java
+++ src/main/java/com/training2/bms/MessagingApiAppApplication.java
@@ -1,10 +1,13 @@
 package com.training2.bms;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MessagingApiAppApplication {
+	@GetMapping("greeting")
 	public void printGreeting()
 	{
 		System.out.println("Rhuthika");
